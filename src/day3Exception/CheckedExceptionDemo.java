7775e6b5971ea1335cc26dabfd148c3fdceb446b
Dhoddi Mallikarjuna Reddy <dhoddireddy@gmail.com>
2026-04-01 11:54:55 +0530

    Updated Threading topic

---
+++ src/day3Exception/CheckedExceptionDemo.java
@@ -0,0 +1,12 @@
+package day3Exception;
+
+public class CheckedExceptionDemo {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
---
+++ src/day3MultiThreading/ThreadsDemo.java
@@ -0,0 +1,37 @@
+//package com.ibm.demo.day3.multithreading;
+//
+//public class ThreadsDemo extends Thread {
+//
+//	void printNums() {
+//		for (int i = 1; i <= 10; i++) {
+//			try {
+//				Thread.sleep(250);
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//			System.out.println(i);
+//		}
+//	}
+//
+//	@Override
+//	public void start() {
+//		this.printNums();
+//	}
+//}
+
+
+package day3MultiThreading;
+
+public class ThreadsDemo {
+
+	void printNums() {
+		for (int i = 1; i <= 10; i++) {
+			try {
+				Thread.sleep(250);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(i);
+		}
+	}
+}
