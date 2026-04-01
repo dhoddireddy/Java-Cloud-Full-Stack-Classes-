ed289f7f634b2d624ac33e125087c1fed7bbb012
Dhoddi Mallikarjuna Reddy <dhoddireddy@gmail.com>
2026-04-01 12:52:57 +0530

    Day 3 as of 12.52

---
+++ src/day3Innerclass/Calc.java
@@ -0,0 +1,5 @@
+package day3Innerclass;
+
+public class Calc {
+
+}
--- src/day3MultiThreading/MultiThreading.java
+++ src/day3MultiThreading/MultiThreading.java
@@ -15,6 +15,8 @@ public class MultiThreading {
 }
 
 
+
+
 //package com.ibm.demo.day3.multithreading;
 //
 //public class MultiThreadingDemo {
